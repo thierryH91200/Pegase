e8fbb2cfcedd8619ab73bcfad08d7f8a77e30779
thierryH91200 <thierry.hentic@gmail.com>
2020-02-15 20:22:11 +0100

    Initial Commit

---
+++ Pegase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pegase
+//
+//  Created by thierry hentic on 15/02/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pegase/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  Pegase
+//
+//  Created by thierry hentic on 15/02/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
